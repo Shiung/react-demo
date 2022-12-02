32e1c31f8ddf448f52570fcd4199188e08337947
FE-Jack <jacks@innotech.me>
2022-12-03 00:37:06 +0800

    add sass

---
+++ src/components/Test/index.tsx
@@ -0,0 +1,12 @@
+import styles from './test.module.scss'
+
+const Test = () => {
+  return (
+    <div className={styles.test}>
+      test
+      <div className={styles.title}>title</div>
+    </div>
+  )
+}
+
+export default Test
